2e5799d848045eb2ec5f911eceacc17e1ceccc86
Harshit Verma <145488053+harshit4v@users.noreply.github.com>
2025-03-18 23:17:33 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+WINDOW_SIZE = "1200x900"
+GANTT_WINDOW_SIZE = "1000x600"
+DEFAULT_THEME = "radiance"
+THEMES = ["radiance", "equilux", "arc", "breeze"]
+LIGHT_BG = "#f0f8ff"
+DARK_BG = "#2e2e2e"
+LIGHT_CHART_BG = "#e6f0ff"
+DARK_CHART_BG = "#3c3f41"
+DEFAULT_QUANTUM = 2
